f79fcdb2c948dbd7c1fa09feffe7592b3cba9da6
Mike Kovacina <michael.kovacina@gmail.com>
2019-11-21 18:08:57 -0500

    new message

--- sample1722/Program.cs
+++ sample1722/Program.cs
@@ -6,7 +6,7 @@ namespace sample1722
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
         }
     }
 }
